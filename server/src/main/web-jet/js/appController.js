4c03bd0416e3712cc6e3797fb7c608b9990c0a9a
Tim Middleton <tim.middleton@oracle.com>
2020-08-13 16:44:51 +0800

    initial changes

---
+++ server/src/main/web-jet/js/appController.js
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+/*
+ * Main controller for rest-examples application.
+ */
+define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojknockouttemplateutils', 'ojs/ojcorerouter', 'ojs/ojmodulerouter-adapter', 'ojs/ojknockoutrouteradapter',
+        'ojs/ojurlparamadapter', 'ojs/ojresponsiveutils', 'ojs/ojresponsiveknockoututils', 'ojs/ojarraydataprovider',
+        'ojs/ojoffcanvas', 'ojs/ojmodule-element', 'ojs/ojknockout'],
+    function (ko, moduleUtils, KnockoutTemplateUtils, CoreRouter, ModuleRouterAdapter, KnockoutRouterAdapter,
+              UrlParamAdapter, ResponsiveUtils, ResponsiveKnockoutUtils, ArrayDataProvider,
+              OffcanvasUtils) {
+        function ControllerViewModel() {
+            var self = this;
+            this.KnockoutTemplateUtils = KnockoutTemplateUtils;
+
+            self.aboutDialogOpen = function () {
+                document.getElementById('aboutDialog').open();
+            };
+
+            // Handle announcements sent when pages change, for Accessibility.
+            this.manner = ko.observable('polite');
+            this.message = ko.observable();
+            announcementHandler = (event) => {
+                this.message(event.detail.message);
+                this.manner(event.detail.manner);
+            };
+
+            document.getElementById('globalBody').addEventListener('announce', announcementHandler, false);
+
+            // Media queries for responsive layouts
+            const smQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+            this.smScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+            const mdQuery = ResponsiveUtils.getFrameworkQuery(ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+            this.mdScreen = ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+
+            let navData = [
+                { path: '', redirect: 'page1' },
+                {
+                    path: 'page1',
+                    detail: {
+                        label: 'Tasks',
+                        iconClass: 'oj-navigationlist-item-icon fa fa-list'
+                    }
+                }
+            ];
+
+            // Router setup
+            let router = new CoreRouter(navData, {
+                urlAdapter: new UrlParamAdapter()
+            });
+            router.sync();
+
+            this.moduleAdapter = new ModuleRouterAdapter(router);
+            this.selection = new KnockoutRouterAdapter(router);
+
+            // Setup the navDataProvider with the routes, excluding the first redirected
+            // route.
+            this.navDataProvider = new ArrayDataProvider(navData.slice(1), {keyAttributes: "path"});
+
+            // Drawer
+            // Close offcanvas on medium and larger screens
+            this.mdScreen.subscribe(() => { OffcanvasUtils.close(this.drawerParams);});
+            this.drawerParams = {
+                displayMode: 'push',
+                selector: '#navDrawer',
+                content: '#pageContent'
+            };
+            // Called by navigation drawer toggle button and after selection of nav drawer item
+            this.toggleDrawer = () => {
+                this.navDrawerOn = true;
+                return OffcanvasUtils.toggle(this.drawerParams);
+            };
+
+            // Header
+            // Application Name used in Branding Area
+            self.appName = ko.observable('Coherence To Do List Example');
+            // User Info used in Global Navigation area
+
+            // Footer
+            function footerLink(name, id, linkTarget) {
+                this.name = name;
+                this.linkId = id;
+                this.linkTarget = linkTarget;
+            }
+
+            // Footer
+            this.footerLinks = [
+                new footerLink('Coherence Community', 'cohCommunity', 'https://coherence.community/'),
+                new footerLink('About Oracle', 'aboutOracle', 'http://www.oracle.com/us/corporate/index.html#menu-about'),
+                new footerLink('Contact Us', 'contactUs', 'http://www.oracle.com/us/corporate/contact/index.html'),
+                new footerLink('Legal Notices', 'legalNotices', 'http://www.oracle.com/us/legal/index.html'),
+                new footerLink('Terms Of Use', 'termsOfUse', 'http://www.oracle.com/us/legal/terms/index.html'),
+                new footerLink('Your Privacy Rights', 'yourPrivacyRights', 'http://www.oracle.com/us/legal/privacy/index.html')
+            ];
+        }
+
+        return new ControllerViewModel();
+    });
---
+++ server/src/main/web-jet/js/viewModels/page1.js
@@ -0,0 +1,242 @@
+/*
+ * Copyright (c) 2019, 2020, Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
+
+/*
+ * ViewModel for Server Sent Events page.
+ */
+define(['knockout', 'ojs/ojbootstrap', 'ojs/ojarraydataprovider', 'ojs/ojvalidation-base', 'ojs/ojdatacollection-utils', 'jquery', 'ojs/ojknockout',
+        'ojs/ojlabel', 'ojs/ojtable', 'ojs/ojinputtext', 'ojs/ojformlayout', 'ojs/ojdatetimepicker', 'ojs/ojselectcombobox', 'ojs/ojradioset',
+        'ojs/ojdialog', 'ojs/ojbutton'],
+    function (ko, Bootstrap, ArrayDataProvider, ValidationBase, DataCollectionEditUtils, $) {
+
+        function Page1ViewModel() {
+            var root = this;
+
+            // the full list of tasks
+            this.taskLoadedArray = ko.observableArray([]);
+
+            // the filtered list off which the table is driven
+            this.taskObservableArray = ko.observableArray([]);
+            this.dataprovider = new ArrayDataProvider(this.taskObservableArray, {keyAttributes: 'id'});
+            this.editRow = ko.observable();
+            this.newTask = ko.observable();
+            this.itemsLeft = ko.observable("0 Items left");
+            this.currentSelection = ko.observable("all");
+
+            this.completionList = ko.observableArray([
+                { value: "true",  label: "Completed" },
+                { value: "false", label: "Active" }
+            ]);
+
+            // function to log an error
+            root.logError = function(message, jqXHR) {
+                console.log(message + ", Response is " + jqXHR.status + " - " + jqXHR.statusText)
+            };
+
+            // apply the filter to the loaded data
+            root.applyFilter = function() {
+                var queryCompleted = root.currentSelection();
+                queryCompleted = queryCompleted === "all" ? undefined : queryCompleted;
+                
+                // filter the data from the taskLoadedArray into the taskObservableArray
+                var filteredData = [];
+                root.taskLoadedArray().forEach(function (entry) {
+                    if (queryCompleted === undefined || queryCompleted === entry.completed) {
+                        filteredData.push(entry);
+                    }
+                });
+                root.taskObservableArray(filteredData);
+                document.getElementById('taskTable').refresh();
+                root.updateItemsLeft();
+            };
+
+            root.updateItemsLeft = function() {
+                var counter = 0;
+                root.taskLoadedArray().forEach(function (entry) {
+                    if (!entry.completed) {
+                        counter++;
+                    }
+                });
+
+                root.itemsLeft(counter + (counter === 1 ? " Item " : " Items ") + " left");
+            };
+
+            // refresh the task list
+            root.reloadData = function() {
+                $.ajax({
+                    url: '/api/tasks',
+                    type: 'GET',
+                    dataType: 'json',
+                    error: function (jqXHR, exception) {
+                        root.logError("Unable to retrieve data", jqXHR);
+                    },
+                    success: function (data) {
+                        root.taskLoadedArray(data);
+                        root.applyFilter();
+                    }
+                });
+
+                root.updateItemsLeft();
+            };
+
+            // update a task
+            root.updateTask = function(id, description, completed) {
+                var updatedTask = {
+                    "id": id,
+                    "description": description,
+                    "completed": completed
+                };
+
+                $.ajax({
+                    url: '/api/tasks/' + id,
+                    type: 'PUT',
+                    data: JSON.stringify(updatedTask),
+                    dataType: 'json',
+                    contentType: "application/json; charset=utf-8",
+                    error: function (jqXHR, exception) {
+                        root.logError("Unable to update task with id=" + id, jqXHR);
+                    },
+                    success: root.reloadData
+                });
+            };
+
+            // delete a task
+            root.deleteTask = function (id) {
+                $.ajax({
+                    url: '/api/tasks/' + id,
+                    type: 'DELETE',
+                    dataType: 'json',
+                    contentType: "application/json; charset=utf-8",
+                    error: function (jqXHR, exception) {
+                        root.logError("Unable to delete task with id=" + id, jqXHR);
+                    },
+                    success: root.reloadData
+                });
+            };
+
+            root.addTask = function () {
+                var description = root.newTask();
+                if (description === "" || description === undefined) {
+                    alert('Please enter a task description');
+                } else {
+                    var newTask = {
+                        "description": description
+                    };
+
+                    $.ajax({
+                        url: '/api/tasks',
+                        type: 'POST',
+                        data: JSON.stringify(newTask),
+                        dataType: 'json',
+                        contentType: "application/json; charset=utf-8",
+                        error: function (jqXHR, exception) {
+                            root.logError("Unable to create task with description =" + description, jqXHR);
+                        },
+                        success: function () {
+                            root.reloadData();
+                            root.newTask("");
+                        }
+                    });
+                }
+            };
+
+            root.clearCompleted = function () {
+                $.ajax({
+                    url: '/api/tasks',
+                    type: 'DELETE',
+                    dataType: 'json',
+                    contentType: "application/json; charset=utf-8",
+                    error: function (jqXHR, exception) {
+                        root.logError("Unable to clear completed tasks", jqXHR);
+                    },
+                    success: function () {
+                        root.reloadData();
+                    }
+                });
+            };
+
+            // respond to a change in the radio button
+            root.currentSelection.subscribe(root.applyFilter);
+
+            // row handling functions
+            this.beforeRowEditEndListener = function (event) {
+                var detail = event.detail;
+                if (DataCollectionEditUtils.basicHandleRowEditEnd(event, detail) === false) {
+                    event.preventDefault();
+                } else {
+                    var updatedData = event.target.getDataForVisibleRow(detail.rowContext.status.rowIndex).data;
+                    // document.getElementById('rowDataDump').value = (JSON.stringify(updatedData));
+                    // save the updated department
+                    console.log("Updated " + updatedData.description);
+                    root.updateTask(updatedData.id, updatedData.description, updatedData.completed);
+                }
+            };
+
+            // process an incoming event and update the clients view of the data
+            root.processEvent = function(type, data) {
+                if (type === 'insert') {
+                    root.taskLoadedArray.push(data);
+                } else if (type === 'delete') {
+                    root.taskLoadedArray.remove(function (item) {
+                        return item.id === data.id;
+                    });
+                } else if (type === 'update') {
+                    var newArray = [];
+                    root.taskLoadedArray().forEach(function(item) {
+                        var newItem = item;
+                        if (newItem.id === data.id) {
+                            newItem.completed = data.completed;
+                            newItem.description = data.description;
+                        }
+                        newArray.push(newItem);
+                    });
+                    root.taskLoadedArray(newArray);
+                }
+                /// finally apply the filter on the updated data set
+                root.applyFilter();
+            };
+
+            this.handleUpdate = function (event, context) {
+                root.editRow({rowKey: context.key});
+            }.bind(this);
+
+            this.handleDone = function (event, context) {
+                root.editRow({rowKey: null});
+            }.bind(this);
+
+            this.handleDelete = function (event, context) {
+                console.log("delete task " + context.key);
+                root.deleteTask(context.key);
+            }.bind(this);
+
+            // Display message if we are running under IE
+            if (typeof (bIsIE) != 'undefined') {
+                alert('Internet Explorer does not support Server Sent Events.\n' +
+                    'Refer to http://www.w3schools.com/html/html5_serversentevents.asp');
+            }
+
+            root.reloadData();
+
+            // setup the SSE Events
+            var eventSourceTask = new EventSource('/api/tasks/events');
+
+            eventSourceTask.addEventListener('insert', function (event) {
+                root.processEvent('insert', JSON.parse(event.data));
+            });
+
+            eventSourceTask.addEventListener('update', function (event) {
+                root.processEvent('update', JSON.parse(event.data));
+            });
+
+            eventSourceTask.addEventListener('delete', function (event) {
+                root.processEvent('delete', JSON.parse(event.data));
+            });
+        }
+
+        return new Page1ViewModel();
+    }
+);

4ca40bee72a05d24217d51674aa8ac73641d4274
Tim Middleton <tim.middleton@oracle.com>
2020-08-19 08:41:11 +0800

    Minor correction to license headers

--- server/src/main/web/react/src/components/App.js
+++ server/src/main/web/react/src/components/App.js
@@ -1,9 +1,9 @@
 /*
- Copyright (c) 2020, Oracle and/or its affiliates.
-
- Licensed under the Universal Permissive License v 1.0 as shown at
- http://oss.oracle.com/licenses/upl.
-*/
+ * Copyright (c) 2020 Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * http://oss.oracle.com/licenses/upl.
+ */
 
 import React from 'react'
 import PropTypes from 'prop-types'

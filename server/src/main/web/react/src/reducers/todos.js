f6765dbc997b9abdffa4a995da282024a21399a6
Dhiru Pandey <dhiru.pandey@oracle.com>
2020-08-31 12:19:55 -0700

    Fixes: - fixed the URLs in the THIRD_PARTY_LICENSES.txt so that they point to resolvable URLs with the versions of the dependencies that are being used - fixed the URL in license copyright headers from http:// to https:// - added attribution for Coherence CE in THIRD_PARTY_LICENSES.txt

--- server/src/main/web/react/src/reducers/todos.js
+++ server/src/main/web/react/src/reducers/todos.js
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
- * http://oss.oracle.com/licenses/upl.
+ * https://oss.oracle.com/licenses/upl.
  */
 
 import { INIT_TODOS, ADD_TODO, DELETE_TODO, UPDATE_TODO, COMPLETE_TODO, CLEAR_COMPLETED } from '../constants/ActionTypes'

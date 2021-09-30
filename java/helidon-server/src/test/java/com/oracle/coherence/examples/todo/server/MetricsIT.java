06d0b0f8d108d5588286e4c56f13f180c26664d5
Vaso Putica <vaso.putica@oracle.com>
2021-09-30 09:22:24 +0200

    Add copyright header

--- java/helidon-server/src/test/java/com/oracle/coherence/examples/todo/server/MetricsIT.java
+++ java/helidon-server/src/test/java/com/oracle/coherence/examples/todo/server/MetricsIT.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2021 Oracle and/or its affiliates.
+ *
+ * Licensed under the Universal Permissive License v 1.0 as shown at
+ * https://oss.oracle.com/licenses/upl.
+ */
+
 package com.oracle.coherence.examples.todo.server;
 
 import io.helidon.microprofile.server.Server;

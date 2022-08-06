6e22e361953396d4100318dd6f13cc7329940553
Gunnar Hillert <gunnar.hillert@oracle.com>
2022-08-05 21:38:38 -1000

    Cleanup

--- java/spring-server/src/main/java/com/oracle/coherence/examples/todo/server/graphql/TaskQueryController.java
+++ java/spring-server/src/main/java/com/oracle/coherence/examples/todo/server/graphql/TaskQueryController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, Oracle and/or its affiliates.
+ * Copyright (c) 2021, 2022, Oracle and/or its affiliates.
  *
  * Licensed under the Universal Permissive License v 1.0 as shown at
  * https://oss.oracle.com/licenses/upl.
@@ -35,12 +35,6 @@ public class TaskQueryController
     public Task findTask(@Argument String id)
         {
         return taskService.find(id);
-        }
-
-        @QueryMapping
-        public String hello()
-        {
-            return "shit";
         }
 
     /**
